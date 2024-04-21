0f0d74d43529a634dd59a6e96d80a036ba649fd6
Oleksii Dmytriiev <sysqwerty1984@gmail.com>
2024-04-21 16:34:38 +0300

    chore

--- js/colorPallleteParentListener.js
+++ js/colorPallleteParentListener.js
@@ -1,11 +1,11 @@
-const colorPalette = document.querySelector(".color-palette");
-const output = document.querySelector(".output");
+const colorPalette = document.querySelector('.color-palette');
+const output = document.querySelector('.output');
 
-colorPalette.addEventListener("click", selectColor);
+colorPalette.addEventListener('click', selectColor);
 
 // This is where delegation «magic» happens
 function selectColor(event) {
-  if (event.target.nodeName !== "BUTTON") {
+  if (event.target.nodeName !== 'BUTTON') {
     return;
   }
 
@@ -18,23 +18,23 @@ function selectColor(event) {
 createPaletteItems();
 
 function createPaletteItems() {
-  console.log("lodash: ", _);
+  console.log('lodash: ', _);
   const items = [];
   for (let i = 0; i < 60; i++) {
     const color = getRandomHexColor();
-    const item = document.createElement("button");
-    item.type = "button";
+    const item = document.createElement('button');
+    item.type = 'button';
     item.dataset.color = color;
     item.style.backgroundColor = color;
-    item.classList.add("item");
+    item.classList.add('item');
     items.push(item);
   }
   colorPalette.append(...items);
 }
 
 function getRandomHexColor() {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
+  const letters = '0123456789ABCDEF';
+  let color = '#';
 
   for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
